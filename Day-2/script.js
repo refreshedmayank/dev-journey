bb93cb3ea9f804a66638b5b6736c0998198ad0c7
refreshedmayank <refreshedmayank@gmail.com>
2026-03-20 17:20:23 +0530

    added new

--- Day-2/script.js
+++ Day-2/script.js
@@ -43,4 +43,43 @@ let a = 0;{
     a++;
   }
 }
-console.log(document);
+// arrow functions
+const greet = (Name) => console.log(`Hello ${Name}, How are you?`);
+greet("Mayank");
+greet("Rohit");
+
+const add = (x,y) => console.log(x+y);
+add(2,3);
+const Birthday = (Name, Age) => console.log(`Happy Birthday to you ${Name}! You are now ${Age} years old!`);
+Birthday("Mayank", 21);
+Birthday("Rohit", 22);
+// arrays
+let Fruits = ["Apple", "Banana", "Mango", "Grapes"];
+console.log(Fruits)
+console.log(Fruits[0]);
+console.log(Fruits[1])
+console.log(Fruits[3])
+console.log(Fruits.length);
+// adding the objects
+Fruits.push("Orange");
+//removing the objects in the end of the array
+Fruits.pop();
+console.log(Fruits)
+Fruits.unshift("Pineapple");
+console.log(Fruits);
+Fruits.shift();
+console.log(Fruits)
+//find the index of the element
+console.log(Fruits.indexOf("Mango"));
+// cheaking if the element is present in the array or not
+console.log(Fruits.includes("Banana"));
+console.log(Fruits.includes("Orange"));
+// looping through the array
+Fruits.forEach(fruit => console.log(fruit));
+//mapping the array
+const numbers = [1,2,3,45,6,7,8,9,10];
+const squaredNumbers = numbers.map(num => num*num);
+console.log(squaredNumbers);
+//filtering the array
+const biggerThank5 = numbers.filter(num => num>5);
+console.log(biggerThank5);
