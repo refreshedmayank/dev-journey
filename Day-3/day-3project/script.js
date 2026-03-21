97e8c293458a1de040de6662c2fd0f50e2823121
refreshedmayank <refreshedmayank@gmail.com>
2026-03-21 18:16:35 +0530

    day 3 added

---
+++ Day-3/day-3project/script.js
@@ -0,0 +1,5 @@
+const aboutuslink = document.getElementById("About-Us");
+const newsletterlink = document.getElementById("Newsletter");
+aboutuslink.addEventListener("click", function(){
+  window.location.href = "About.html";
+})
